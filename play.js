ade8b75c2b0b55924bf13e1e52669152e9226665
Sophia <chiaki.katsuno@live.ca>
2023-09-20 23:41:34 -0700

    stdin setup

--- play.js
+++ play.js
@@ -2,5 +2,28 @@
 
 const { connect } = require("./client");
 
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  // event listener for stdin
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  if (key === "\u0003") {
+    // terminates game from ctrl + c input
+    process.exit();
+  }
+};
+
+const stdin = setupInput();
+
 console.log("Connecting ...");
 connect();
